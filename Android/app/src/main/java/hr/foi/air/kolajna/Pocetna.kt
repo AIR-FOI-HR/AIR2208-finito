404433ae906163d27b4ccd0ae160a8aae3c6359c
DESKTOP-VPJK79O\Patricija <pcobovic@student.foi.hr>
2023-02-06 12:06:40 +0100

    Dodana prijava putem e-maila i lozinke.

---
+++ Android/app/src/main/java/hr/foi/air/kolajna/Pocetna.kt
@@ -0,0 +1,11 @@
+package hr.foi.air.kolajna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pocetna : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pocetna)
+    }
+}
