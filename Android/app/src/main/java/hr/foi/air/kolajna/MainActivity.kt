68b0ae51c582a9d483d9985e120272bb1dd578ef
pcobovic <pcobovic@student.foi.hr>
2023-02-06 15:54:10 +0100

    Zaboravljena lozinka

--- Android/app/src/main/java/hr/foi/air/kolajna/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/kolajna/MainActivity.kt
@@ -1,14 +1,24 @@
 package hr.foi.air.kolajna
 
+
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        auth = FirebaseAuth.getInstance()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -44,6 +54,41 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
+        forgetpassword.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Zaboravljena lozinka")
+            val view = layoutInflater.inflate(R.layout.dialog_forgot_password, null)
+
+            val username = view.findViewById<EditText>(R.id.idEmail)
+
+            builder.setView(view)
+            builder.setPositiveButton("Ponovno postavi", { _, _ ->
+                forgotPassword(username)
+            })
+            builder.setNegativeButton("Zatvori", { _, _ -> })
+
+            builder.show()
+        }
+    }
+    private fun forgotPassword(username : EditText){
+        auth.sendPasswordResetEmail(username.getText().toString())
+            .addOnCompleteListener(OnCompleteListener<Void?> { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Poslan je e-mail s oporavkom lozinke.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                }
+            })
+            .addOnFailureListener {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Ne postoji registrirani korisnik s ovom e-mail adresom.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 
 }
