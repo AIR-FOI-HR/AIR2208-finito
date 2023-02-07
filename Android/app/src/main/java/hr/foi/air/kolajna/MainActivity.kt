5503ccdb718012a93c856c115ce8e7b8af6da81c
pcobovic <pcobovic@student.foi.hr>
2023-02-07 12:23:32 +0100

    Promjene u dizajnu Google i Facebook Sign In.

--- Android/app/src/main/java/hr/foi/air/kolajna/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/kolajna/MainActivity.kt
@@ -1,37 +1,50 @@
 package hr.foi.air.kolajna
 
-
-
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import hr.foi.air.kolajna.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
 
 
-
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var binding: ActivityMainBinding
 
+    private var callbackManager:CallbackManager?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         auth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -76,10 +89,10 @@ class MainActivity : AppCompatActivity() {
             val username = view.findViewById<EditText>(R.id.idEmail)
 
             builder.setView(view)
-            builder.setPositiveButton("Ponovno postavi", { _, _ ->
+            builder.setPositiveButton("Ponovno postavi") { _, _ ->
                 forgotPassword(username)
-            })
-            builder.setNegativeButton("Zatvori", { _, _ -> })
+            }
+            builder.setNegativeButton("Zatvori") { _, _ -> }
 
             builder.show()
         }
@@ -94,8 +107,67 @@ class MainActivity : AppCompatActivity() {
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        binding.googleBtn.setOnClickListener(){
+        binding.googleBtn.setOnClickListener {
             signIn()
+        }
+
+        //za Facebook SignIn
+
+        printKeyHash()
+
+        facebookBtn.setOnClickListener{
+            facebookSignIn()
+        }
+    }
+
+    private fun facebookSignIn() {
+
+        LoginManager.getInstance()
+        .logInWithReadPermissions(this, listOf("public_profile", "email", "user_friends"))
+        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+            override fun onCancel() {
+                Toast.makeText(this@MainActivity, "Login otkazan", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onError(error: FacebookException) {
+                Toast.makeText(this@MainActivity, error.message, Toast.LENGTH_LONG).show()
+            }
+
+            override fun onSuccess(result: LoginResult) {
+                handleFacebookAccessToken(result.accessToken)
+            }
+
+        })
+    }
+
+    private fun handleFacebookAccessToken(accessToken: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(accessToken.token)
+        auth.signInWithCredential(credential)
+            .addOnFailureListener{ e->
+                Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+            }
+            .addOnSuccessListener { result->
+                val email = result.user?.email
+                Toast.makeText(this, "Prijavljeni ste s e-mail adresom $email", Toast.LENGTH_LONG).show()
+            }
+    }
+
+
+    @SuppressLint("PackageManagerGetSignatures")
+    private fun printKeyHash() {
+        try{
+            val info = packageManager.getPackageInfo("hr.foi.air.kolajna", PackageManager.GET_SIGNATURES)
+            for(signature in info.signatures){
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.e("KEYHASH", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            }
+        }
+        catch (e:PackageManager.NameNotFoundException){
+
+        }
+        catch (e:NoSuchAlgorithmException){
+
         }
     }
 
@@ -104,6 +176,7 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -119,6 +192,8 @@ class MainActivity : AppCompatActivity() {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e)
             }
+        }else{
+            callbackManager?.onActivityResult(requestCode, resultCode, data)
         }
     }
 
@@ -151,16 +226,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun forgotPassword(username : EditText){
-        auth.sendPasswordResetEmail(username.getText().toString())
+        auth.sendPasswordResetEmail(username.text.toString())
             .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Poslan je e-mail s oporavkom lozinke.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else {
-                }
+                if (task.isSuccessful) Toast.makeText(
+                    this@MainActivity,
+                    "Poslan je e-mail s oporavkom lozinke.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
             .addOnFailureListener {
                 Toast.makeText(
